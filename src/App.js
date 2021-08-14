ac2bb13d39df4fb55506a62bfa3c6d698c9e45ca
Kevin Mallari <kevinmallari62@gmail.com>
2021-08-14 11:22:16 +0800

    Finished product.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ import "./App.css";
 
 function App() {
   const operations = ["/", "*", "+", "-"];
-  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
   const [equation, setEquation] = useState([]);
   const [tempAnswer, setTempAnswer] = useState("");
   const [answer, setAnswer] = useState("");
   const [input, setInput] = useState("");
   const [decimal, setDecimal] = useState(false);
+  const [deleted, setDeleted] = useState(true);
 
   const handleClick = (e) => {
+    console.log(e.currentTarget.value);
     if (e.target.value !== "AC" && e.target.value !== "b") {
       setInput(e.target.value);
     }
@@ -30,12 +32,22 @@ function App() {
       setDecimal(false);
       setInput("");
       return;
-    } else if (e.target.value === "b") {
+    } else if (e.currentTarget.value === "b") {
       if (equation[equation.length - 1] === ".") {
         setDecimal(false);
       }
+      console.log("equation", equation);
+      console.log("answer", [answer]);
+      if (equation.length === 1) {
+        setEquation([]);
+        setTempAnswer("");
+        setAnswer("");
+        setDecimal(false);
+        setInput("");
+        console.log("SUPPUPUU");
+      }
       equation.splice(-1);
-      setEquation((equation) => [...equation]);
+      setDeleted(!deleted);
       return;
     } else if (
       operations.includes(e.target.value) &&
@@ -43,7 +55,7 @@ function App() {
     ) {
       equation.splice(-1);
     } else if (e.target.value === "=") {
-      if (operations.includes(equation[equation.length-1])) {
+      if (operations.includes(equation[equation.length - 1])) {
         equation.splice(-1);
       }
       if (operations.includes(equation[0]) && equation[0] !== "-") {
@@ -51,8 +63,8 @@ function App() {
       }
       setTempAnswer(evaluate(equation.join("")));
       setEquation([answer]);
+      setDecimal(false);
     } else if (operations.includes(e.target.value) && answer !== "") {
-      console.log("does this run");
       setTempAnswer("");
     } else if (answer !== "" && numbers.includes(e.target.value)) {
       setEquation([]);
@@ -76,17 +88,23 @@ function App() {
     if (answer !== "" && !operations.includes(equation[equation.length - 1])) {
       setEquation([answer]);
     }
-  }, [tempAnswer, answer]);
-
-  // console.log("equation", equation);
-  // console.log("answer", answer);
+  }, [tempAnswer, answer, deleted, decimal]);
 
   return (
-    <div className="grid grid-row-2 justify-center content-center h-screen v-screen bg-gray-100">
+    <div className="grid justify-center content-center h-screen v-screen bg-gray-100">
       <div className="shadow-2xl">
         <Display input={input} equation={equation.join("")} />
         <Buttons handleClick={handleClick} />
       </div>
+      <footer className="relative top-10 text-center font-semibold font-mono text-gray-500 overflow-y-hidden">
+        © Made by{" "}
+        <a
+          className="hover:text-gray-700 hover:underline transition-all"
+          href="https://github.com/kmallari"
+        >
+          kmallari
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,28 +1,28 @@
-const Buttons = ({ handleClick, handleEquate }) => {
+const Buttons = ({ handleClick }) => {
   return (
     <div className="grid grid-cols-4 justify-items-center items-center max-w-md bg-gray-100 font-sans text-xl">
-      <button value={"AC"} onClick={handleClick} id="clear" className="font-bold text-gray-500 bg-gray-100 w-48 h-24 col-start-1 col-end-3">AC</button>
-      <button value={"b"} onClick={handleClick} id="backspace" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 flex items-center justify-center">
+      <button value={"AC"} onClick={handleClick} id="clear" className="hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-48 h-24 col-start-1 col-end-3">AC</button>
+      <button value={"b"} onClick={handleClick} id="backspace" className="hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 flex items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z" />
         </svg>
       </button>
-      <button value={"/"} onClick={handleClick} id="divide" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">÷</button>
-      <button value={1} onClick={handleClick} id="one" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">1</button>
-      <button value={2} onClick={handleClick} id="two" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">2</button>
-      <button value={3} onClick={handleClick} id="three" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">3</button>
-      <button value={"*"} onClick={handleClick} id="multiply" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">×</button>
-      <button value={4} onClick={handleClick} id="four" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">4</button>
-      <button value={5} onClick={handleClick} id="five" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">5</button>
-      <button value={6} onClick={handleClick} id="six" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">6</button>
-      <button value={"+"} onClick={handleClick} id="add" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">+</button>
-      <button value={7} onClick={handleClick} id="seven" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">7</button>
-      <button value={8} onClick={handleClick} id="eight" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">8</button>
-      <button value={9} onClick={handleClick} id="nine" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">9</button>
-      <button value={"-"} onClick={handleClick} id="minus" className="font-bold text-gray-500 bg-gray-100 w-24 h-24 ">-</button>
-      <button value={0} onClick={handleClick} id="zero" className="col-start-2 col-end-3 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">0</button>
-      <button value={"."} onClick={handleClick} id="decimal" className="col-start-3 col-end-4 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">.</button>
-      <button value={"="} onClick={handleClick} id="equals" className="col-start-4 col-end-5 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">=</button>
+      <button value={"/"} onClick={handleClick} id="divide" className="hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">÷</button>
+      <button value={1} onClick={handleClick} id="one" className="hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">1</button>
+      <button value={2} onClick={handleClick} id="two" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">2</button>
+      <button value={3} onClick={handleClick} id="three" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">3</button>
+      <button value={"*"} onClick={handleClick} id="multiply" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">×</button>
+      <button value={4} onClick={handleClick} id="four" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">4</button>
+      <button value={5} onClick={handleClick} id="five" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">5</button>
+      <button value={6} onClick={handleClick} id="six" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">6</button>
+      <button value={"+"} onClick={handleClick} id="add" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">+</button>
+      <button value={7} onClick={handleClick} id="seven" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">7</button>
+      <button value={8} onClick={handleClick} id="eight" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">8</button>
+      <button value={9} onClick={handleClick} id="nine" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">9</button>
+      <button value={"-"} onClick={handleClick} id="minus" className=" hover:bg-white transition-all font-bold text-gray-500 bg-gray-100 w-24 h-24 ">-</button>
+      <button value={0} onClick={handleClick} id="zero" className=" hover:bg-white transition-all col-start-2 col-end-3 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">0</button>
+      <button value={"."} onClick={handleClick} id="decimal" className=" hover:bg-white transition-all col-start-3 col-end-4 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">.</button>
+      <button value={"="} onClick={handleClick} id="equals" className=" hover:bg-white transition-all col-start-4 col-end-5 font-bold text-gray-500 bg-gray-100 w-24 h-24 ">=</button>
     </div>
   );
 };
